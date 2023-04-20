056dde26762b1ad883339fa401e94fa1d2d2caf4
rhs99 <33574220+rhs99@users.noreply.github.com>
2023-04-21 01:50:13 +0600

    convert to ts
    make server to support ts

---
+++ lt-server/src/index.ts
@@ -0,0 +1,43 @@
+import express, { Application, Request, Response } from "express";
+import mongoose, { ConnectOptions } from "mongoose";
+import cors from 'cors';
+import bodyParser from "body-parser";
+
+const app: Application = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({
+    message: "Hello World!",
+  });
+});
+
+
+const PORT = 5000;
+
+try {
+  app.listen(PORT, (): void => {
+    console.log(`Connected successfully on port ${PORT}`);
+  });
+} catch (error: any) {
+  console.error(`Error occured: ${error.message}`);
+}
+
+const DB_URL = "mongodb://mongo:27017/lt-db";
+
+const connectDB = async () => {
+  await mongoose
+    .connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions)
+    .then(() => console.log("Mongo connected successfully"))
+    .catch((e) => {
+      console.log(e.message);
+    });
+};
+
+connectDB();
