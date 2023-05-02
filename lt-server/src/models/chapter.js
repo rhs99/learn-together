e3da85cefdbc6327b59a33d0ad3c1e47789ffb0c
TahmidAnjum <tahmidanjum320@gmail.com>
2023-05-02 21:29:51 +0600

    Added unique-validator for subject and chapter

--- lt-server/src/models/chapter.js
+++ lt-server/src/models/chapter.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const chapterSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -6,6 +7,8 @@ const chapterSchema = new mongoose.Schema({
 });
 
 chapterSchema.index({ name: 1, subject: 1 }, { unique: true });
+chapterSchema.plugin(uniqueValidator);
+
 const Chapter = mongoose.model('Chapter', chapterSchema);
 
 module.exports = Chapter;
--- lt-server/src/models/subject.js
+++ lt-server/src/models/subject.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const subjectSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -6,6 +7,8 @@ const subjectSchema = new mongoose.Schema({
 });
 
 subjectSchema.index({ name: 1, class: 1 }, { unique: true });
+subjectSchema.plugin(uniqueValidator);
+
 const Subject = mongoose.model('Subject', subjectSchema);
 
 module.exports = Subject;
