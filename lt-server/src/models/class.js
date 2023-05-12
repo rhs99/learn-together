3ee5528064eff65de70ec952006fb6779e84ad99
Rakibul Hasan <rhrana99@gmail.com>
2023-05-13 00:53:20 +0600

    remove redundant code

--- lt-server/src/models/class.js
+++ lt-server/src/models/class.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
 
 const classSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
     isDeleted: { type: Boolean, default: false },
 });
-classSchema.plugin(uniqueValidator);
 
 const Class = mongoose.model('Class', classSchema);
 
--- lt-server/src/models/privilege.js
+++ lt-server/src/models/privilege.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
 
 const privilegeSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
     isDeleted: { type: Boolean, default: false },
 });
-privilegeSchema.plugin(uniqueValidator);
 
 const Privilege = mongoose.model('Privilege', privilegeSchema);
 
