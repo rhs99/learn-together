29462d959149152a930e7c060f1e3d42ed60047e
TahmidAnjum <tahmidanjum320@gmail.com>
2023-05-17 08:07:51 +0600

    Updated header ui

--- lt-client/src/components/MainNavigation/MainNavigation.tsx
+++ lt-client/src/components/MainNavigation/MainNavigation.tsx
@@ -1,5 +1,12 @@
-import { NavLink, redirect } from 'react-router-dom';
+import { Link, redirect } from 'react-router-dom';
 import { useContext } from 'react';
+import { Stack } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import SchoolRoundedIcon from '@mui/icons-material/SchoolRounded';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import Button from '@mui/material/Button';
 import AuthContext from '../../store/auth';
 
 import './_index.scss';
@@ -14,42 +21,40 @@ const MainNavigation = () => {
   };
 
   return (
-    <nav className="cl-mainNavigation">
-      <ul className="container">
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}
-            end
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/about"
-            className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}
-          >
-            About
-          </NavLink>
-        </li>
-        {!isLoggedIn && (
-          <li>
-            <NavLink to="/users/login">Login</NavLink>
-          </li>
-        )}
-        {!isLoggedIn && (
-          <li className="item">
-            <NavLink to="/users/signup">Signup</NavLink>
-          </li>
-        )}
-        {isLoggedIn && (
-          <li>
-            <button onClick={logoutHandler}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Stack direction="row" spacing={2}>
+            <SchoolRoundedIcon fontSize="large" />
+            <Button color="inherit" component={Link} to="/">
+              Home
+            </Button>
+            <Button color="inherit" component={Link} to="/about">
+              About
+            </Button>
+          </Stack>
+
+          <Stack direction="row" spacing={2} sx={{ mr: 2, ml: 'auto' }}>
+            {!isLoggedIn && (
+              <Button color="inherit" component={Link} to="/users/login">
+                Login
+              </Button>
+            )}
+            {!isLoggedIn && (
+              <Button color="inherit" component={Link} to="/users/signup">
+                Signup
+              </Button>
+            )}
+            {isLoggedIn && (
+              <Button color="inherit" onClick={logoutHandler}>
+                Logout
+              </Button>
+            )}
+            <AccountCircleRoundedIcon fontSize="large" />
+          </Stack>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
