614e569fd60c4db7129caddfc6a9d98fdb4f2494
Rakibul Hasan <rhrana99@gmail.com>
2023-05-19 22:44:47 +0600

    fix chapter detail styling

--- lt-client/src/pages/ChapterDetail/ChapterDetail.tsx
+++ lt-client/src/pages/ChapterDetail/ChapterDetail.tsx
@@ -5,6 +5,9 @@ import Util from '../../utils';
 import { Question } from '../../types';
 import QuestionCard from '../../components/QuestionCard/QuestionCard';
 import AuthContext from '../../store/auth';
+import {Typography ,Button } from '@mui/material';
+
+import './_index.scss';
 
 const ChapterDetail = () => {
   const [questions, setQuestions] = useState<Question[]>([]);
@@ -25,9 +28,17 @@ const ChapterDetail = () => {
   };
 
   return (
-    <div>
-      {isLoggedIn && <button onClick={handleAskQuestion}>Ask Question</button>}
-      <p>All Questions</p>
+    <div className="cl-ChapterDetail">
+      <div className='heading'>
+        <Typography variant='h6'>All Questions</Typography>
+        {isLoggedIn && (
+          <div className="ask-btn">
+            <Button variant="outlined" onClick={handleAskQuestion}>
+              Ask Question
+            </Button>
+          </div>
+        )}
+      </div>
       {questions.map((question) => (
         <QuestionCard key={question._id} question={question} qdClickable={true} />
       ))}
