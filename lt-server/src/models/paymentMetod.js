9268e1995fac1da669899bf7acdf61e2bc610d71
TahmidAnjum <tahmidanjum320@gmail.com>
2024-02-29 21:38:59 +0600

    changed according to the review

--- lt-server/src/models/paymentMetod.js
+++ lt-server/src/models/paymentMetod.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const paymentMethodSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
-    donations: [{ type: mongoose.Types.ObjectId, ref: 'Donation' }],
 });
 
 const PaymentMethod = mongoose.model('PaymentMethod', paymentMethodSchema);
