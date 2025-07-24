510a2558d06860c271cda85beec83cbc4b7980ec
rhs99 <rakibul.sarker@optimizely.com>
2025-07-24 19:39:03 +0600

    fix sorting

--- lt-server/src/services/answer.js
+++ lt-server/src/services/answer.js
@@ -4,89 +4,187 @@ const User = require('../models/user');
 const Notification = require('../models/notification');
 const Utils = require('../common/utils');
 const { UnauthorizedError } = require('../common/error');
-
 const connectedUsers = require('../common/connected-users');
 
+const NOTIFICATION_TYPES = {
+    NEW_ANSWER: 'new_answer',
+};
+
+const SOCKET_EVENTS = {
+    NEW_ANSWER: 'new answer',
+};
+
+const normalizeImageLocations = (imageLocations = []) => {
+    return imageLocations.map((fileName) => Utils.getFileUrl(fileName));
+};
+
+const validateUserOwnership = (resourceUserName, currentUserName, errorMessage) => {
+    if (resourceUserName !== currentUserName) {
+        throw new UnauthorizedError(errorMessage || 'Unauthorized: You do not have permission to perform this action');
+    }
+};
+
 const addNewAnswer = async (body) => {
-    const user = await User.findById(body.user).exec();
-    const question = await Question.findById(body.question).exec();
+    if (!body.user || !body.question) {
+        throw new Error('User ID and Question ID are required');
+    }
+
     body.imageLocations = body.imageLocations || [];
 
-    let answer;
-    if (body._id !== '') {
-        answer = await Answer.findOne({ _id: body._id }).exec();
-        if (answer.userName !== user.userName) {
-            throw new UnauthorizedError();
-        }
-        if (answer.imageLocations.length > 0 && body.imageLocations.length > 0) {
-            Utils.deleteFile(answer.imageLocations);
-        }
-        answer.details = body.details || answer.details;
-        answer.imageLocations = body.imageLocations.length === 0 ? answer.imageLocations : body.imageLocations;
-        await answer.save();
-    } else {
-        delete body._id;
-        delete body.user;
-        body.userName = user.userName;
+    const [user, question] = await Promise.all([
+        User.findById(body.user).exec(),
+        Question.findById(body.question).exec(),
+    ]);
 
-        answer = new Answer(body);
-        answer = await answer.save();
-        user.answers.push(answer._id);
-        await user.save();
-        question.answers.push(answer._id);
-        await question.save();
+    if (!user) throw new Error('User not found');
+    if (!question) throw new Error('Question not found');
+
+    const answer =
+        body._id && body._id !== ''
+            ? await updateExistingAnswer(body, user)
+            : await createNewAnswer(body, user, question);
+
+    await notifyQuestionOwner(question, user);
+
+    return answer;
+};
+
+const updateExistingAnswer = async (body, user) => {
+    const answer = await Answer.findById(body._id).exec();
+
+    if (!answer) {
+        throw new Error('Answer not found');
     }
-    const qOwner = await User.findOne({ userName: question.userName }).exec();
+
+    validateUserOwnership(answer.userName, user.userName, 'You can only edit your own answers');
+
+    if (answer.imageLocations.length > 0 && body.imageLocations.length > 0) {
+        Utils.deleteFile(answer.imageLocations);
+    }
+
+    answer.details = body.details || answer.details;
+    answer.imageLocations = body.imageLocations.length === 0 ? answer.imageLocations : body.imageLocations;
+
+    await answer.save();
+    return answer;
+};
+
+const createNewAnswer = async (body, user, question) => {
+    const answerData = {
+        ...body,
+        userName: user.userName,
+        _id: undefined,
+        user: undefined,
+    };
+
+    const answer = new Answer(answerData);
+    const savedAnswer = await answer.save();
+
+    await Promise.all([
+        User.findByIdAndUpdate(user._id, { $push: { answers: savedAnswer._id } }),
+        Question.findByIdAndUpdate(question._id, { $push: { answers: savedAnswer._id } }),
+    ]);
+
+    return savedAnswer;
+};
+
+const notifyQuestionOwner = async (question, answerAuthor) => {
+    if (question.userName === answerAuthor.userName) {
+        return;
+    }
+
+    const questionOwner = await User.findOne({ userName: question.userName }).exec();
+
+    if (!questionOwner) {
+        console.error(`Question owner not found: ${question.userName}`);
+        return;
+    }
 
     const notification = new Notification({
-        userId: qOwner._id,
-        type: 'new_answer',
+        userId: questionOwner._id,
+        type: NOTIFICATION_TYPES.NEW_ANSWER,
         details: question._id,
         read: false,
     });
+
     await notification.save();
 
-    const socket = connectedUsers.get(qOwner.userName);
+    const socket = connectedUsers.get(questionOwner.userName);
     if (socket) {
-        socket.send('new answer');
+        socket.send(SOCKET_EVENTS.NEW_ANSWER);
     }
 };
 
 const getAnswer = async (answerId) => {
-    const answer = await Answer.findOne({ _id: answerId }).exec();
-    answer.imageLocations = answer.imageLocations.map((fileName) => {
-        return Utils.getFileUrl(fileName);
-    });
-    return answer;
+    if (!answerId) {
+        throw new Error('Answer ID is required');
+    }
+
+    const answer = await Answer.findById(answerId).exec();
+
+    if (!answer) {
+        throw new Error('Answer not found');
+    }
+
+    return {
+        ...answer.toObject(),
+        imageLocations: normalizeImageLocations(answer.imageLocations),
+    };
 };
 
 const getAllAnswers = async (questionId) => {
-    const answers = await Answer.find({ question: questionId }).exec();
-    const resp = answers.map((answer) => {
-        answer.imageLocations = answer.imageLocations.map((fileName) => {
-            return Utils.getFileUrl(fileName);
-        });
-        return {
-            ...answer._doc,
-        };
-    });
-    return resp;
+    if (!questionId) {
+        throw new Error('Question ID is required');
+    }
+
+    const answers = await Answer.find({ question: questionId }).sort({ createdAt: -1 }).exec();
+
+    return answers.map((answer) => ({
+        ...answer.toObject(),
+        imageLocations: normalizeImageLocations(answer.imageLocations),
+    }));
 };
 
 const deleteAnswer = async (answerId, userId) => {
-    const answer = await Answer.findById(answerId).exec();
-    const question = await Question.findById(answer.question).exec();
-    const user = await User.findById(userId).exec();
-
-    if (answer.userName !== user.userName && question.userName !== user.userName) {
-        throw new Error('unauth');
+    if (!answerId || !userId) {
+        throw new Error('Answer ID and User ID are required');
     }
-    question.answers = question.answers.filter((q) => JSON.stringify(q) !== JSON.stringify(answer._id));
-    await question.save();
-    user.answers = user.answers.filter((item) => JSON.stringify(item) !== JSON.stringify(answerId));
-    await user.save();
+
+    const answer = await Answer.findById(answerId).exec();
+    if (!answer) {
+        throw new Error('Answer not found');
+    }
+
+    const [question, user] = await Promise.all([
+        Question.findById(answer.question).exec(),
+        User.findById(userId).exec(),
+    ]);
+
+    if (!question) throw new Error('Question not found');
+    if (!user) throw new Error('User not found');
+
+    const isAnswerOwner = answer.userName === user.userName;
+    const isQuestionOwner = question.userName === user.userName;
+
+    if (!isAnswerOwner && !isQuestionOwner) {
+        throw new UnauthorizedError('You can only delete your own answers or answers to your questions');
+    }
+
+    await Promise.all([
+        Question.findByIdAndUpdate(question._id, { $pull: { answers: answerId } }),
+        isAnswerOwner ? User.findByIdAndUpdate(userId, { $pull: { answers: answerId } }) : Promise.resolve(),
+    ]);
+
     await Answer.deleteOne({ _id: answerId }).exec();
-    Utils.deleteFile(answer.imageLocations);
+
+    if (answer.imageLocations && answer.imageLocations.length > 0) {
+        Utils.deleteFile(answer.imageLocations);
+    }
 };
 
-module.exports = { addNewAnswer, getAllAnswers, getAnswer, deleteAnswer };
+module.exports = {
+    addNewAnswer,
+    getAllAnswers,
+    getAnswer,
+    deleteAnswer,
+};
--- lt-server/src/services/question.js
+++ lt-server/src/services/question.js
@@ -5,180 +5,272 @@ const Chapter = require('../models/chapter');
 const User = require('../models/user');
 const Utils = require('../common/utils');
 
+const DEFAULT_PAGE_NUMBER = 1;
+const DEFAULT_PAGE_SIZE = 10;
+const MAX_PAGE_SIZE = 10;
+
+const SORT_FIELDS = {
+    upVote: 'upVote',
+    downVote: 'downVote',
+    time: 'createdAt',
+    vote: 'voteDifference',
+};
+
+const SORT_ORDERS = {
+    asc: 1,
+    desc: -1,
+};
+
+const FILTER_TYPES = {
+    all: 'all',
+    favourite: 'favourite',
+    mine: 'mine',
+};
+
+const compareObjectIds = (id1, id2) => {
+    return String(id1) === String(id2);
+};
+
+const normalizeImageLocations = (imageLocations = []) => {
+    return imageLocations.map((fileName) => Utils.getFileUrl(fileName));
+};
+
+const createSortStage = (sortBy, sortOrder) => {
+    const field = SORT_FIELDS[sortBy] || 'createdAt';
+    const order = SORT_ORDERS[sortOrder] || SORT_ORDERS.desc;
+    return { [field]: order };
+};
+
+const buildMatchStage = (chapterQuestions, filterBy, user, tagIds) => {
+    const baseMatch = { _id: { $in: chapterQuestions } };
+
+    switch (filterBy) {
+        case FILTER_TYPES.favourite:
+            if (!user) throw new Error('User required for favourite filter');
+            return {
+                $and: [baseMatch, { _id: { $in: user.favourites } }],
+            };
+
+        case FILTER_TYPES.mine:
+            if (!user) throw new Error('User required for mine filter');
+            return { ...baseMatch, userName: user.userName };
+
+        default:
+            return tagIds.length > 0 ? { ...baseMatch, 'tags._id': { $in: tagIds } } : baseMatch;
+    }
+};
+
+const enrichQuestionWithFavouriteStatus = (question, user, filterBy) => {
+    const isFavourite =
+        filterBy === FILTER_TYPES.favourite ||
+        (user && user.favourites.some((favId) => compareObjectIds(favId, question._id)));
+
+    return {
+        ...question,
+        isFavourite,
+        imageLocations: normalizeImageLocations(question.imageLocations),
+    };
+};
+
 const addNewQuestion = async (body) => {
     const user = await User.findById(body.user).exec();
+    if (!user) throw new Error('User not found');
 
     body.imageLocations = body.imageLocations || [];
     body.tags = body.tags || [];
 
-    if (body._id !== '') {
-        const question = await Question.findOne({ _id: body._id }).exec();
-        if (question.userName !== user.userName) {
-            throw new Error('unauth');
-        }
-        if (question.imageLocations.length > 0 && body.imageLocations.length > 0) {
-            Utils.deleteFile(question.imageLocations);
-        }
-
-        question.details = body.details || question.details;
-        question.imageLocations = body.imageLocations.length === 0 ? question.imageLocations : body.imageLocations;
-        question.tags = body.tags;
-        await question.save();
-        return;
+    if (body._id && body._id !== '') {
+        return await updateExistingQuestion(body, user);
     }
 
-    delete body._id;
-    delete body.user;
-    body.userName = user.userName;
+    return await createNewQuestion(body, user);
+};
 
-    let question = new Question(body);
-    question = await question.save();
+const updateExistingQuestion = async (body, user) => {
+    const question = await Question.findById(body._id).exec();
+    if (!question) throw new Error('Question not found');
 
-    user.questions.push(question._id);
-    await user.save();
-    const chapter = await Chapter.findById(body.chapter).exec();
-    chapter.questions.push(question._id);
-    await chapter.save();
+    if (question.userName !== user.userName) {
+        throw new Error('Unauthorized: You can only edit your own questions');
+    }
+
+    if (question.imageLocations.length > 0 && body.imageLocations.length > 0) {
+        Utils.deleteFile(question.imageLocations);
+    }
+
+    question.details = body.details || question.details;
+    question.imageLocations = body.imageLocations.length === 0 ? question.imageLocations : body.imageLocations;
+    question.tags = body.tags;
+
+    await question.save();
+};
+
+const createNewQuestion = async (body, user) => {
+    const questionData = {
+        ...body,
+        userName: user.userName,
+        _id: undefined,
+        user: undefined,
+    };
+
+    const question = new Question(questionData);
+    const savedQuestion = await question.save();
+
+    await Promise.all([
+        User.findByIdAndUpdate(user._id, { $push: { questions: savedQuestion._id } }),
+        Chapter.findByIdAndUpdate(body.chapter, { $push: { questions: savedQuestion._id } }),
+    ]);
+
+    return savedQuestion;
 };
 
 const getAllQuestions = async (body, query) => {
-    const pageNumber = query.pageNumber || 1;
-    let pageSize = query.pageSize || 10;
-    if (pageSize > 10) {
-        pageSize = 10;
-    }
-    let key = 'createdAt',
-        val = -1;
+    const pageNumber = Math.max(parseInt(query.pageNumber) || DEFAULT_PAGE_NUMBER, 1);
+    const pageSize = Math.min(parseInt(query.pageSize) || DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE);
 
-    if (query.sortBy && query.sortOrder) {
-        switch (query.sortBy) {
-            case 'upVote':
-                key = 'upVote';
-                break;
-            case 'downVote':
-                key = 'downVote';
-                break;
-            case 'time':
-                key = 'createdAt';
-                break;
-            case 'vote':
-                key = 'vote';
-                break;
-        }
-        switch (query.sortOrder) {
-            case 'desc':
-                val = -1;
-                break;
-            case 'asc':
-                val = 1;
-                break;
-        }
-    }
-    const sortOption = { [key]: val };
-    const filterBy = query.filterBy || 'all';
+    const filterBy = query.filterBy || FILTER_TYPES.all;
 
-    let user = null;
-    if (body.user) {
-        user = await User.findById(body.user).exec();
-    }
+    const [user, chapter] = await Promise.all([
+        body.user ? User.findById(body.user).exec() : null,
+        Chapter.findById(body.chapterId).exec(),
+    ]);
 
-    const chapter = await Chapter.findById(body.chapterId).exec();
-    let tagIds = [];
+    if (!chapter) throw new Error('Chapter not found');
 
-    if (body.tagIds && body.tagIds.length > 0) {
-        tagIds = body.tagIds.map((_id) => new mongoose.Types.ObjectId(_id));
-    }
+    const tagIds = (body.tagIds || []).map((id) => new mongoose.Types.ObjectId(id));
 
-    let q = { _id: { $in: chapter.questions } };
-
-    if (filterBy === 'favourite') {
-        q = { $and: [{ _id: { $in: chapter.questions } }, { _id: { $in: user.favourites } }] };
-    } else if (filterBy === 'mine') {
-        q['userName'] = { $eq: user.userName };
-    }
-
-    if (tagIds.length > 0) {
-        q['tags._id'] = { $in: tagIds };
-    }
-
-    const totalFilteredCount = await Question.countDocuments(q).exec();
-
-    const allQuestions = await Question.find(q)
-        .sort(sortOption)
-        .skip((pageNumber - 1) * pageSize)
-        .limit(pageSize)
-        .exec();
-
-    const searchResponse = allQuestions.map((question) => {
-        question.imageLocations = question.imageLocations.map((fileName) => {
-            return Utils.getFileUrl(fileName);
-        });
-
-        if (filterBy === 'favourite') {
-            return {
-                ...question._doc,
-                isFavourite: true,
-            };
-        }
-        const isPresent = user
-            ? user.favourites.some((questionId) => JSON.stringify(questionId) === JSON.stringify(question._id))
-            : false;
-        return {
-            ...question._doc,
-            isFavourite: isPresent,
-        };
+    const pipeline = buildAggregationPipeline({
+        chapterQuestions: chapter.questions,
+        filterBy,
+        user,
+        tagIds,
+        sortBy: query.sortBy,
+        sortOrder: query.sortOrder,
+        pageNumber,
+        pageSize,
     });
 
-    return { totalCount: totalFilteredCount, paginatedResults: searchResponse };
+    const [result] = await Question.aggregate(pipeline).exec();
+
+    const totalCount = result?.metadata[0]?.totalCount || 0;
+    const questions = result?.data || [];
+
+    const paginatedResults = questions.map((question) => enrichQuestionWithFavouriteStatus(question, user, filterBy));
+
+    return { totalCount, paginatedResults };
+};
+
+const buildAggregationPipeline = ({
+    chapterQuestions,
+    filterBy,
+    user,
+    tagIds,
+    sortBy,
+    sortOrder,
+    pageNumber,
+    pageSize,
+}) => {
+    const matchStage = buildMatchStage(chapterQuestions, filterBy, user, tagIds);
+    const sortStage = createSortStage(sortBy, sortOrder);
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const pipeline = [{ $match: matchStage }];
+
+    if (sortBy === 'vote') {
+        pipeline.push({
+            $addFields: {
+                voteDifference: { $subtract: ['$upVote', '$downVote'] },
+            },
+        });
+    }
+
+    pipeline.push(
+        { $sort: sortStage },
+        {
+            $facet: {
+                metadata: [{ $count: 'totalCount' }],
+                data: [{ $skip: skipAmount }, { $limit: pageSize }],
+            },
+        },
+    );
+
+    return pipeline;
 };
 
 const getQuestion = async (questionId) => {
-    const question = await Question.findOne({ _id: questionId }).exec();
-    question.imageLocations = question.imageLocations.map((fileName) => {
-        return Utils.getFileUrl(fileName);
-    });
-    return question;
+    const question = await Question.findById(questionId).exec();
+    if (!question) throw new Error('Question not found');
+
+    return {
+        ...question.toObject(),
+        imageLocations: normalizeImageLocations(question.imageLocations),
+    };
 };
 
 const deleteQuestion = async (questionId, userId) => {
-    const user = await User.findById(userId).exec();
-    const question = await Question.findById(questionId).exec();
-    if (question.userName !== user.userName) {
-        throw new Error('unauth');
+    const [user, question] = await Promise.all([User.findById(userId).exec(), Question.findById(questionId).exec()]);
+
+    if (!user || !question) {
+        throw new Error('User or question not found');
     }
-    const allFiles = question.imageLocations;
-    const promises = question.answers.map((_id) => {
-        return Answer.findOneAndDelete({ _id: _id }).exec();
-    });
-    const answers = await Promise.all(promises);
-    answers.forEach((answer) => {
-        allFiles.push(...answer.imageLocations);
-    });
-    const chapter = await Chapter.findById(question.chapter).exec();
-    chapter.questions = chapter.questions.filter((item) => JSON.stringify(item) !== JSON.stringify(questionId));
-    await chapter.save();
-    user.questions = user.questions.filter((item) => JSON.stringify(item) !== JSON.stringify(questionId));
-    await user.save();
+
+    if (question.userName !== user.userName) {
+        throw new Error('Unauthorized: You can only delete your own questions');
+    }
+
+    const filesToDelete = [...question.imageLocations];
+
+    if (question.answers.length > 0) {
+        const deletedAnswers = await Promise.all(
+            question.answers.map((answerId) => Answer.findByIdAndDelete(answerId).exec()),
+        );
+
+        deletedAnswers.forEach((answer) => {
+            if (answer?.imageLocations) {
+                filesToDelete.push(...answer.imageLocations);
+            }
+        });
+    }
+
+    await Promise.all([
+        Chapter.findByIdAndUpdate(question.chapter, { $pull: { questions: questionId } }),
+        User.findByIdAndUpdate(userId, { $pull: { questions: questionId } }),
+    ]);
+
     await Question.deleteOne({ _id: questionId }).exec();
-    if (allFiles.length > 0) {
-        Utils.deleteFile(allFiles);
+
+    if (filesToDelete.length > 0) {
+        Utils.deleteFile(filesToDelete);
     }
 };
 
 const addToFavourite = async (body) => {
-    const user = await User.findById(body.user).exec();
-    const isPresent = user.favourites.some(
-        (questionId) => JSON.stringify(questionId) === JSON.stringify(body.questionId),
-    );
-    if (!isPresent) {
-        user.favourites.push(body.questionId);
-    } else {
-        user.favourites = user.favourites.filter(
-            (questionId) => JSON.stringify(questionId) !== JSON.stringify(body.questionId),
-        );
+    const { user: userId, questionId } = body;
+
+    if (!userId || !questionId) {
+        throw new Error('User ID and Question ID are required');
     }
+
+    const user = await User.findById(userId).exec();
+    if (!user) throw new Error('User not found');
+
+    const isFavourite = user.favourites.some((favId) => compareObjectIds(favId, questionId));
+
+    if (isFavourite) {
+        user.favourites = user.favourites.filter((favId) => !compareObjectIds(favId, questionId));
+    } else {
+        user.favourites.push(questionId);
+    }
+
     await user.save();
-    return { favourite: !isPresent };
+
+    return { favourite: !isFavourite };
 };
 
-module.exports = { addNewQuestion, getAllQuestions, getQuestion, deleteQuestion, addToFavourite };
+module.exports = {
+    addNewQuestion,
+    getAllQuestions,
+    getQuestion,
+    deleteQuestion,
+    addToFavourite,
+};
