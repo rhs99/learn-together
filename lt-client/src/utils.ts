08f59c4256a4b44c098580e2f56e7eab8c778f2d
rhs99 <rakibul.sarker@optimizely.com>
2026-01-23 23:07:27 +0600

    update ws url

--- lt-client/src/utils.ts
+++ lt-client/src/utils.ts
@@ -3,6 +3,9 @@ const Util = {
     // SERVER_URL: 'http://localhost:5000',
     SERVER_URL: 'http://ballast.proxy.rlwy.net:54817',
     // SERVER_URL: 'https://nurturing-empathy-production.up.railway.app',
+    // WEBSOCKET_URL: 'ws://localhost:5000',
+    WEBSOCKET_URL: 'ws://ballast.proxy.rlwy.net:54817',
+    // WEBSOCKET_URL: 'wss://nurturing-empathy-production.up.railway.app',
     CLIENT_URL: 'http://localhost:3000',
   },
 };
