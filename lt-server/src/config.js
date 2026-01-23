dc680088aff50e59a4c440f851b9dc019674f1de
rhs99 <rakibul.sarker@optimizely.com>
2026-01-23 17:36:09 +0600

    use supabase

--- lt-server/src/config.js
+++ lt-server/src/config.js
@@ -1,9 +1,10 @@
 const Config = {
     LT_HOST: 'http://localhost:3000',
 
-    MINIO_HOST: 'play.min.io',
-    MINIO_PORT: 9000,
-    MINIO_BUCKET: 'lt-bucket',
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    SUPABASE_STORAGE_BUCKET: process.env.SUPABASE_STORAGE_BUCKET || 'lt-bucket',
     REDIS_HOST: 'lt-cache',
     REDIS_PORT: 6379,
 };
